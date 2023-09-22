cd025fab09d1f31b7a315317192d68963309aa56
aldmiranda <federicomiranda_03@hotmail.com>
2023-09-22 17:05:50 -0300

    Subida de primeros ejercicios

---
+++ Ejercicio1.java
@@ -0,0 +1,8 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int numero1, numero2;
+        numero1 = 5; numero2 = 2;
+        var resultado = numero1 + numero2;
+        System.out.println("el resultado es: " + resultado);
+        }
+}
