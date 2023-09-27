e6a36880a2023195956bb09e80e87999e0739461
aldmiranda <federicomiranda_03@hotmail.com>
2023-09-27 18:51:15 -0300

    haciendo mas ejercicios

---
+++ Ejercicio4.java
@@ -0,0 +1,14 @@
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        int numero1, numero2;
+        numero1 = 10;
+        numero2 = 5;
+
+        var resultado = numero1 + numero2;
+
+        System.out.println(String.format("El %d %s es divisble por el %d y el resutlado es %d", numero1, numero1%numero2 == 0? "" : "no", numero2, resultado));
+    }
+}
+
+
+
