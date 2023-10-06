ec9bc0227b6a1423af7ce4167c4bd64c0161bcb6
aldmiranda <federicomiranda_03@hotmail.com>
2023-10-06 18:17:41 -0300

    Avanzando con Tp1 seccion Caballero

--- Caballero/Ejercicio2caballero.java
+++ Caballero/Ejercicio2caballero.java
@@ -5,10 +5,24 @@ package Caballero;
 // tasa de cambio fija. Por ejemplo, si la tasa de cambio es de 1 dólar = 20 pesos, y el
 // usuario ingresa 50 dólares, el programa deberá mostrar 1000 pesos.
 
+import java.util.Scanner;
 
 public class Ejercicio2caballero {
     public static void main(String[] args) {
-               
+        int pesos;
+        int TASA_FIJA;
+        double dolarConversion;
 
+        TASA_FIJA = 885;
+                    
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un importes en pesos: ");
+        pesos = scanner.nextInt();
+
+        dolarConversion = (double) pesos / TASA_FIJA;
+
+        scanner.close();
+        System.out.printf("%d pesos equivalen a %.2f dolares, teniendo en cuenta un valor de %d%n Blue", pesos, dolarConversion, TASA_FIJA);
+     
     }
 }
